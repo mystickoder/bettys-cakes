afb3375136343a9b55cc772552838ee0afe64c08
mystickoder <89115482+mystickoder@users.noreply.github.com>
2021-09-30 15:04:29 -0400

    Add files via upload
    Betty's Cakes a local bakery

---
+++ bettys-cakes.py
@@ -0,0 +1,24 @@
+print("Lets' see if Betty's Cakes have what I like. ")
+
+cakes = ['chocolate','alpine','banna cream','butter whip','coconut cake']
+
+if 'coconut cake' not in cakes:
+    print("Lets look somewhere else. ")
+
+else:
+    print("Wow, We have found what we were looing for. ")
+
+if "found what you are looking for":
+    print("I also want ice cream.")
+
+if "ice cream request":
+    print("Lets invite over some friends! ")
+
+else:
+    print("I have no one to celebrate with")
+
+if "friends invited":
+    print("Great a party with cake and ice cream for everyone")
+
+
+    print("Thanks for bringing me to Betty's Cakes")
